79a0e059af894276e09546ce88093a907312838f
Luqianyou <757628817@qq.com>
2022-10-12 13:04:37 +0800

    axios module no exports error

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import React, { useEffect } from 'react'
 import './App.sass'
+import axios from 'axios'
 import Child from './Child'
+
 function App() {
-  useEffect(() => {}, [])
+  useEffect(() => {
+    axios
+      .get('https://i.maoyan.com/api/mmdb/movie/v3/list/hot.json?ct=%E9%83%BD%E6%B1%9F%E5%A0%B0&ci=576&channelId=4')
+      .then((res) => {
+        console.log(res.data)
+      })
+  }, [])
 
   return (
     <div className='box'>
       123
       <ul>
         <li>11111</li>
-        <li>22222</li>
-        <li>33333</li>
+        <li>22222</li>0<li>33333</li>
       </ul>
       <Child />
     </div>
